1f96a6b625eba1e8373c90594a9417f713f5ab94
Mingfeng Zhong <mingfengz1678@gmail.com>
2025-09-09 15:17:39 -0400

    Activity done

--- swen-340-project/Core/Src/printf.c
+++ swen-340-project/Core/Src/printf.c
@@ -2,7 +2,14 @@
 #include <stdio.h>
 #include <stdarg.h>
 
-char buffer[80];
+char buffer[120];
+
+int puts(const char* string) {
+	int ret = strlen (string);
+	USART_Write (USART2, (unsigned char*) string, ret);
+	USART_Write (USART2, (unsigned char*) "\n", ret);
+	return ret + 1;
+}
 
 int printf(const char *format, ...) {
 	va_list aptr;
